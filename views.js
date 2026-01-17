437336fa42366acb198ea471f105d3f4c414f9f9
Offsec <117650498+prokaungminthant@users.noreply.github.com>
2026-01-17 17:51:02 +0630

    Add files via upload

---
+++ views.js
@@ -0,0 +1,16 @@
+
+import { kv } from '@vercel/kv';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { reset } = req.body || {};
+    if (reset === true) {
+      await kv.set('views', 0);
+      return res.json({ views: 0 });
+    }
+  }
+
+  const views = (await kv.get('views')) ?? 0;
+  const newViews = await kv.incr('views');
+  res.json({ views: newViews });
+}
